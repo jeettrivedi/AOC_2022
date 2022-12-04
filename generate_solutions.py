93c5d6f62242e12419cff16ee2fa8a32177ab17d
jeettrivedi <jtriv@protonmail.com>
2022-12-04 03:23:03 -0500

    Added Day 4 solutions

--- generate_solutions.py
+++ generate_solutions.py
@@ -5,6 +5,8 @@ from aoc_2022.day_2.part_1 import sol as d2p1
 from aoc_2022.day_2.part_2 import sol as d2p2
 from aoc_2022.day_3.part_1 import sol as d3p1
 from aoc_2022.day_3.part_2 import sol as d3p2
+from aoc_2022.day_4.part_1 import sol as d4p1
+from aoc_2022.day_4.part_2 import sol as d4p2
 
 pretty_print_header(1)
 print(f'The solution to day 1 part 1 is {d1p1("data/1/data.txt")}')
@@ -17,3 +19,7 @@ print(f'The solution to day 2 part 2 is {d2p2("data/2/data.txt")}')
 pretty_print_header(3)
 print(f'The solution to day 3 part 1 is {d3p1("data/3/data.txt")}')
 print(f'The solution to day 3 part 2 is {d3p2("data/3/data.txt")}')
+
+pretty_print_header(4)
+print(f'The solution to day 4 part 1 is {d4p1("data/4/data.txt")}')
+print(f'The solution to day 4 part 2 is {d4p2("data/4/data.txt")}')
---
+++ tests/day_4/test_part_2.py
@@ -0,0 +1,4 @@
+from aoc_2022.day_4.part_2 import sol
+
+def test_sol():
+    assert sol('tests/day_4/data.txt') == 4
